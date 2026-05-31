85d8a0a7c5950e475ebb24cdf2b2e15298b9658c
Ayush Chand <aayushchand900@gmail.com>
2026-05-31 16:54:20 +0530

    Login steps definitions

--- Tests/UI_Test/Feature/login.feature
+++ Tests/UI_Test/Feature/login.feature
@@ -5,13 +5,13 @@ Feature: User Authentication tests
     And User click on the login link
 
   Scenario: Login should be success
-    And User enter the username as "ortoni"
-    And User enter the password as "Pass1234"
+    And User enter the username as "Ayush_07"
+    And User enter the password as "Qwerty@123"
     When User click on the login button
     Then Login should be success
 
-  Scenario: Login should not be success
-    Given User enter the username as "koushik"
-    Given User enter the password as "Passkoushik"
-    When User click on the login button
-    But Login should fail
+  # Scenario: Login should not be success
+  #   Given User enter the username as "koushik"
+  #   Given User enter the password as "Passkoushik"
+  #   When User click on the login button
+  #   But Login should fail
--- Tests/UI_Test/steps/login.steps.ts
+++ Tests/UI_Test/steps/login.steps.ts
@@ -1,5 +1,41 @@
-import {Given} from "@cucumber/cucumber"
+import {Given, When, Then,} from "@cucumber/cucumber"
+import {Page, Browser, BrowserContext, chromium, expect } from "playwright/test"
+
+let browser : Browser;
+let context : BrowserContext
+let page : Page;
 
 Given('User navigates to the application', async()=>{
-    console.log ("HI")
+browser = await chromium.launch({headless: false});
+context = await browser.newContext();
+page = await context.newPage();
+await page.goto("https://bookcart.azurewebsites.net/")       
 })
+
+Given('User click on the login link', async function () {
+await page.getByText('Login').first().click()
+});
+        
+Given('User enter the username as {string}', async function (username) {
+await page.getByRole('textbox', {name: "Username"}).fill(username)
+});
+             
+Given('User enter the password as {string}', async function (password) {
+await page.getByRole('textbox', {name: "Password"}).fill(password)
+});
+       
+When('User click on the login button', async function () {
+await page.locator('span').filter({ hasText: 'Login' }).last().click()
+});
+             
+Then('Login should be success', async function () {
+expect(page.getByText('account_circle', { exact: true })).toBeVisible()
+await page.waitForTimeout(3000);
+await browser.close();
+});
+
+//  But('Login should fail', async function () {
+//    expect(page.getByText("")).toBeVisible()
+//  });
+
+        
